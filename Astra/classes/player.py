307872d5c1c13a46299d88d800edbbd3a44d236b
FinkSo <valentin.valleepro@gmail.com>
2023-04-17 20:39:32 +0200

    Good scale modif

--- Astra/classes/player.py
+++ Astra/classes/player.py
@@ -4,7 +4,9 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load("img/ShipTest.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (self.image.get_width() // 2, self.image.get_height() // 2))
+        self.imageWidth = 101.25
+        self.imageHeight = 105.5
+        self.image = pygame.transform.scale(self.image,(int(self.imageWidth), int(self.imageHeight)))
         self.rect = self.image.get_rect(x=x, y=y)
         self.speed = 5
         self.velocity = [0, 0]
