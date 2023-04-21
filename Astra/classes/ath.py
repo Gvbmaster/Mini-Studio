f128d86436e0acfd4c0ce1d9b2a71336c3e9e941
FinkSo <valentin.valleepro@gmail.com>
2023-04-21 13:45:38 +0200

    ATH HEALTH BAR FINISH

--- Astra/classes/ath.py
+++ Astra/classes/ath.py
@@ -3,28 +3,30 @@ from classes.values import *
 
 class ATH():
     def __init__(self):
-        self.healthBar = pygame.image.load("img/healthBar.png").convert_alpha()
-        self.rectHealthBar = self.healthBar.get_rect(x=0, y=0)
-        self.lifePoint = pygame.image.load("img/lifePoint.png").convert_alpha()
+        self.healthBar = pygame.image.load("img/healthBarLowPoly.png").convert_alpha()
+        self.rectHealthBar = self.healthBar.get_rect(x=25, y=25)
+        self.lifePoint = pygame.image.load("img/lifePointLowPoly.png").convert_alpha()
+        self.lifePoint = pygame.transform.scale(self.lifePoint,(169,124))
 
     def update(self):
         pass
 
     def draw(self, screen):
-        screen.blit(self.healthBar, self.rectHealthBar)
-
         if PlayerStats.currentHealth == 1:
-            rectLifePoint1 = self.lifePoint.get_rect(x=50, y=150)
+            rectLifePoint1 = self.lifePoint.get_rect(x=70, y=51)
             screen.blit(self.lifePoint, rectLifePoint1)
         elif PlayerStats.currentHealth == 2:
-            rectLifePoint1 = self.lifePoint.get_rect(x=50, y=150)
-            rectLifePoint2 = self.lifePoint.get_rect(x=50, y=250)
+            rectLifePoint1 = self.lifePoint.get_rect(x=70, y=51)
+            rectLifePoint2 = self.lifePoint.get_rect(x=150,  y=51)
             screen.blit(self.lifePoint, rectLifePoint1)
             screen.blit(self.lifePoint, rectLifePoint2)
         elif PlayerStats.currentHealth == 3:
-            rectLifePoint1 = self.lifePoint.get_rect(x=50, y=150)
-            rectLifePoint2 = self.lifePoint.get_rect(x=50, y=250)
-            rectLifePoint3 = self.lifePoint.get_rect(x=50, y=350)
+            rectLifePoint1 = self.lifePoint.get_rect(x=70, y=51)
+            rectLifePoint2 = self.lifePoint.get_rect(x=150, y=51)
+            rectLifePoint3 = self.lifePoint.get_rect(x=232, y=51)
             screen.blit(self.lifePoint, rectLifePoint1)
             screen.blit(self.lifePoint, rectLifePoint2)
             screen.blit(self.lifePoint, rectLifePoint3)
+        screen.blit(self.healthBar, self.rectHealthBar)
+
+        
