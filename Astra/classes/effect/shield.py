addbc2ce93067fb789a18ee127cac012c136e33f
Guilherme Coelho Vieira <92694963+YtsDarko04@users.noreply.github.com>
2023-04-24 20:16:39 +0200

    Delete Astra directory

--- Astra/classes/effect/shield.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Shield():
-    def __init__(self):
-        self.idEffect = 2
-        self.image = pygame.image.load("img/Shield.png").convert_alpha()
-        self.imageWidth = 151.25
-        self.imageHeight = 146.5
-        self.image = pygame.transform.scale(self.image,(int(self.imageWidth), int(self.imageHeight)))
