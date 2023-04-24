4daa68260698be0c6bc0275c461fd27225d26d88
Tristan BODA <115488503+Kron0l@users.noreply.github.com>
2023-04-24 15:25:58 +0200

    enemy ok

--- Astra/classes/values.py
+++ Astra/classes/values.py
@@ -14,14 +14,16 @@ class PlayerStats:
     #maxSpeed = 10
     attackDamage = 3
     #maxAttackDamage = 6
-    attackSpeed = 1
-    MaxAttackSpeed = 2   
-    
+    attackSpeed = 100 # cadence de tir
+    MaxAttackSpeed = 2
+    attackVelocity = 20 #speed bullet
+    shield = False
+    isPlayerHitable = True
 
 class EnnemieStats:
     currentHealth = 6
     maxHealth = 10
-    speed = 5
+    speed = 2
     maxSpeed = 14
     attackDamage = 1
     maxAttackDamage = 2
@@ -44,8 +46,7 @@ class BossStats:
     maxAttackDamage = 2
     attackSpeed = 1
     MaxAttackSpeed = 2
-
+    
 class ObstacleStats:
     speed = 7
     #maxSpeed = 14
-
