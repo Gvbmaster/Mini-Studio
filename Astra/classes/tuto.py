4670379a968fba37989e9467c6824eb97dc5c5b1
lorenzo-molina <lmolina@gaming.tech>
2023-04-27 01:56:08 +0200

    tuto fini

--- Astra/classes/tuto.py
+++ Astra/classes/tuto.py
@@ -14,9 +14,11 @@ from classes.laser import *
 from classes.enemy import *
 from classes.rectangle import *
 from classes.text import *
+from level1 import Level1
 
 class Tuto:
     def __init__(self, screen):
+#########################################################
         self.screen = screen
         self.running = True
         self.clock = pygame.time.Clock()
@@ -24,21 +26,31 @@ class Tuto:
         self.ath = ATH()
         self.ls = LifeSystem(self)
         self.player = Player(50,500)
-######### OBSTACLE INIT ###############
+        self.all_sprites_layer_2 = pygame.sprite.Group()
+        self.tuto_fini = pygame.USEREVENT + 1000
+###########################################################        
+
+############## OBSTACLE INIT ###############
         self.obstacles = []
         self.obstacle_group = pygame.sprite.Group()
         self.obstacle_y = None
         self.obstacle = None
         self.obstacle_tuto_event = pygame.USEREVENT + 100
+        self.all_sprites_layer_2.add(self.obstacle_group)
 #####################################
 
-######### TEXTE INIT ###################
+############## TEXTE INIT ###################
         self.text_tuto_event_01 = pygame.USEREVENT + 1
         pygame.time.set_timer(self.text_tuto_event_01, 500)
         self.text_tuto_event_02 = pygame.USEREVENT + 2
+        self.text_tuto_event_03 = pygame.USEREVENT + 3
+        self.text_tuto_event_03_depop = pygame.USEREVENT + 33
+        self.text_tuto_event_04 = pygame.USEREVENT + 4
+        self.text_tuto_event_04_depop = pygame.USEREVENT + 44
         self.is_rect_drawable_01 = False
         self.is_rect_drawable_02 = False
         self.is_rect_drawable_03 = False
+        self.is_rect_drawable_04 = False
         self.zoneTexte = Rectangle()
         self.zPress = False
         self.qPress = False
@@ -50,7 +62,6 @@ class Tuto:
         self.text03 = "Attention !!!"
         self.text04 = "Les astéroïdes vous infligeront des dégâts, soyez vigilant !"
         self.text05 = "Pour vous soigner, récupérez des coeurs comme celui-ci !"
-        self.text06 = "Les boucliers vous protègeront, prenez-les dès que possible !"
         self.font = pygame.font.Font('font/TurretRoad-Regular.ttf', 60)
         self.textDraw = None
 #########################################
@@ -59,16 +70,15 @@ class Tuto:
         self.enemy_tuto_event = pygame.USEREVENT +200
         self.enemy = pygame.sprite.Group()
         self.enemyTuto = None
-        self.enemyDeath_tuto_event = pygame.USEREVENT +300
 #######################################################
 
+############# PLAYER INIT #########################
         self.sprite_player = pygame.sprite.Group()
         self.sprite_player.add(self.player)
-        self.all_sprites_layer_2 = pygame.sprite.Group()
-        self.all_sprites_layer_2.add(self.obstacle_group)
-        self.all_sprites_projectilesMC = pygame.sprite.Group()
+###################################################
         
 ############# TIR INIT #################################
+        self.all_sprites_projectilesMC = pygame.sprite.Group()
         self.spacePress = False
         self.last_shot_time = 0
         self.last_shot_timeEn = 0
@@ -94,14 +104,18 @@ class Tuto:
                 self.enemyTuto=Enemy(EnnemieStats.patternSpawn[EnnemieStats.pattern][0][0],EnnemieStats.patternSpawn[EnnemieStats.pattern][0][1])
                 self.enemy.add(self.enemyTuto)
                 self.all_sprites_layer_2.add(self.enemy)
-                pygame.time.set_timer(self.enemy_tuto_event, 0)
+                pygame.time.set_timer(self.enemy_tuto_event, 0)                
             
-            if not(self.enemyTuto in self.enemy) :
-                pygame.time.set_timer(self.enemyDeath_tuto_event, 2000)
-                print("azefzaegvbzaev")
-            
-            if event.type == self.enemyDeath_tuto_event :
-                pygame.time.set_timer(self.enemyDeath_tuto_event, 0)
+        #event d'apparition de la zone de texte n°03
+            if event.type == self.text_tuto_event_03 :
+                pygame.time.set_timer(self.text_tuto_event_03, 0)
+                self.is_rect_drawable_03 = True
+                pygame.time.set_timer(self.text_tuto_event_03_depop, 2000)
+
+        #event de disparition de la zone de texte n°03
+            if event.type == self.text_tuto_event_03_depop :
+                pygame.time.set_timer(self.text_tuto_event_03_depop, 0)
+                self.is_rect_drawable_03 = False
 
         #event d'apparition des obstacles
             if event.type == self.obstacle_tuto_event :
@@ -112,7 +126,21 @@ class Tuto:
                     self.obstacles.append(obstacle)
                     self.all_sprites_layer_2.add(obstacle)
                     self.obstacle_y = self.obstacle_y + 100
-                # pygame.time.set_timer(self.obstacle_tuto_event, 500)
+
+        #event d'apparition de la zone de texte n°04
+            if event.type == self.text_tuto_event_04 :
+                pygame.time.set_timer(self.text_tuto_event_04, 0)
+                self.is_rect_drawable_04 = True
+
+        #event de disparition de la zone de texte n°04
+            if event.type == self.text_tuto_event_04_depop :
+                pygame.time.set_timer(self.text_tuto_event_04_depop, 0)
+                self.is_rect_drawable_04 = False
+                pygame.time.set_timer(self.tuto_fini, 3000)
+
+        #event de lancement du niveau 1
+            if event.type == self.tuto_fini :
+                pass
 
         #touches de déplacement
             keys = pygame.key.get_pressed()
@@ -159,6 +187,7 @@ class Tuto:
                 projectile.kill()
             elif pygame.sprite.spritecollide(projectile, self.enemy, True):
                 projectile.kill()
+                pygame.time.set_timer(self.text_tuto_event_03, 2000)
 
     def update(self):
         self.player.move()
@@ -168,15 +197,17 @@ class Tuto:
             if pygame.sprite.spritecollide(obstacle, self.sprite_player, False):
                 self.ls.healthPlayerUpdate(obstacle)
                 Invicibility.update()
-                print("Player take hit")
-                print(PlayerStats.currentHealth)
+                obstacle.kill()
             if obstacle.rect.x <= 0 - obstacle.imageWidth:
                 obstacle.kill()
                 self.obstacle_group.remove(obstacle)
                 self.obstacles.remove(obstacle)
+                pygame.time.set_timer(self.text_tuto_event_04, 2000)
             else:
-                obstacle.move()
+                obstacle.move()              
+
         if self.enemyTuto!=None:
+            print('zrazdaz')
             self.enemyTuto.move()
 
     def display(self):
@@ -195,14 +226,18 @@ class Tuto:
             self.textDraw = Text(self.font, self.text02, 800)
             Rectangle.draw(self)
             self.textDraw.drawText(self.screen, 550, 180)
-            
             if self.spacePress == True:
                 self.is_rect_drawable_02 = False
-                pygame.time.set_timer(self.obstacle_tuto_event, 2000)
         if self.is_rect_drawable_03 == True:
             self.textDraw = Text(self.font, self.text03, 800)
             Rectangle.draw(self)
             self.textDraw.drawText(self.screen, 550, 180)
+            pygame.time.set_timer(self.obstacle_tuto_event, 2000)
+        if self.is_rect_drawable_04 == True:
+            self.textDraw = Text(self.font, self.text04, 800)
+            Rectangle.draw(self)
+            self.textDraw.drawText(self.screen, 550, 180)
+            pygame.time.set_timer(self.text_tuto_event_04_depop, 2000)
         self.all_sprites_layer_2.draw(self.screen)
         self.player.draw(self.screen)
         self.ath.draw(self.screen)
